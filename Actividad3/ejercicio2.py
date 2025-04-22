364a30c6196f37861ab401e01a8bc49067c98a7d
LuisCReyes <lcreyes@uamv.edu.ni>
2025-04-22 08:20:30 -0600

    Ejercicio2 FInalizado

--- Actividad3/ejercicio2.py
+++ Actividad3/ejercicio2.py
@@ -4,11 +4,11 @@ productos_vendidos = int(input("Ingrese la cantidad de productos vendidos: "))
 suma = productos_recibidos + cantidad_inicial
 inventario_actual = suma - productos_vendidos
 
-print("-" * 45)
+print("-" * 44)
 
 print(f"{"Inventario inicial:":<20} {cantidad_inicial:>20}")
 print(f"{"Productos recibidos:":<20} {productos_recibidos:>20}")
 print(f"{"Productos vendidos:":<20} {productos_vendidos:>20}")
 print(f"{"Inventario final:":<20} {inventario_actual:>20}")
 
-print ("-" * 45)
+print ("-" * 44)
