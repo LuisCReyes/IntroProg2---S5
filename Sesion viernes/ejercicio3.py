03d713f2290adc3825d30a1074ac3f6b2a7d143f
LuisCReyes <lcreyes@uamv.edu.ni>
2025-04-25 08:23:09 -0600

    ejercicio3

---
+++ Sesion viernes/ejercicio3.py
@@ -0,0 +1,39 @@
+print("""Ingrese un número de la siguiente lista:
+                   (1) Lunes
+                   (2) Martes
+                   (3) Miércoles
+                   (4) Jueves
+                   (5) Viernes
+                   (6) Sábado
+                   (7) Domingo """)
+numero = int(input(">"))
+
+while numero != 0:
+    if numero == 1:
+        print("El día de la semana es Lunes")
+    elif numero == 2:
+        print("El día de la semana es Martes")
+    elif numero == 3:
+        print("El día de la semana es Miércoles")
+    elif numero == 4:
+        print("El día de la semana es Jueves")
+    elif numero == 5:
+        print("El día de la semana es Viernes")
+    elif numero == 6:
+        print("El día de la semana es Sábado")
+    elif numero == 7:
+        print("El día de la semana es Domingo")
+    else:
+        print("Número inválido :(")
+       
+    print("Presiona Cero para salir")
+
+    print("""Ingrese un número de la siguiente lista:
+                   (1) Lunes
+                   (2) Martes
+                   (3) Miércoles
+                   (4) Jueves
+                   (5) Viernes
+                   (6) Sábado
+                   (7) Domingo """)
+    numero = int(input(">"))
